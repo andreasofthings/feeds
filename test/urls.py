186fb3a05a13f3988e1d171773bccf09290b7633
Andreas Neumeier <andreas@neumeier.org>
2014-04-29 16:44:23 +0200

    Update urls.py
    Django 1.6 way.

--- test/urls.py
+++ test/urls.py
@@ -5,7 +5,7 @@
 urly.py to allow tests in travis-ci
 """
 
-from django.conf.urls.defaults import patterns, url, include, handler404
+from django.conf.url import patterns, url, include, handler404
 
 urlpatterns = patterns('',
     url(r'^feeds/', include( 'feeds.urls', namespace="planet", app_name="planet")),
