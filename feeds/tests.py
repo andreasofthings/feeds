64a47094222c73cc2b3612e841a8f0175de132b4
Andreas Neumeier <andreas@neumeier.org>
2014-05-03 10:04:49 +0200

    now fix build of docs

--- feeds/tests.py
+++ feeds/tests.py
@@ -69,6 +69,9 @@ class TaskTest(TestCase):
         self.post2.save()
 
     def test_task_time(self):
+        """
+        .. todo:: Needs amqp.
+        """
         dummy.delay(invocation_time=datetime.now())
         dummy(10)
 
