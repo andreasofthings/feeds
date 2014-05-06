e6a41caa96398b8ebae1db36973cc07685b0edc5
Andreas Neumeier <andreas@neumeier.org>
2014-05-06 15:16:43 +0200

    PEP8 and so.

--- feeds/tests.py
+++ feeds/tests.py
@@ -5,7 +5,7 @@
 """
 Tests for the "feeds" app.
 ==========================
-  
+
   :date: 2014-05-03
   :version: 0.1
   :description: Test Cases for :py:mod:`feeds`
@@ -37,6 +37,7 @@ from feeds import ENTRY_NEW, ENTRY_UPDATED, ENTRY_SAME, ENTRY_ERR
 
 from datetime import datetime
 
+
 class ModelTest(TestCase):
     """
     Test Models and their Managers
@@ -52,7 +53,7 @@ class ModelTest(TestCase):
     - :py:mod:`feeds.models.Feed`
     - :py:mod:`feeds.models.Post`
     - :py:mod:`feeds.models.Enclosure`
-    
+
     .. codeauthor:: Andreas Neumeier <andreas@neumeier.org>
     """
     def setUp(self):
@@ -63,15 +64,19 @@ class ModelTest(TestCase):
 
     def test_site(self):
         """
-        Create a :py:mod:`feeds.models.Site` Object and verify it functions properly.
+        Create a :py:mod:`feeds.models.Site` Object and verify
+        it functions properly.
         """
 
         from feeds.models import Site
         s = Site(url="https://angry-planet.com/")
         s.save()
         # self.assertContains( s.get_absolute_url(), s.pk)
-        """.. todo:: self.assertContains won't work for what is being tested here."""
-        self.assertEqual(str(s), "https://angry-planet.com/")
+        """
+        .. todo:: self.assertContains won't work
+        for what is being tested here.
+        """
+        self.assertEqual(str(s), "http://angry-planet.com/")
         """Assert the __str__ representation equals the site-name."""
 
     def test_tag(self):
@@ -118,6 +123,7 @@ class ModelTest(TestCase):
         """
         pass
 
+
 class TaskTest(TestCase):
     """
     Test Tasks
@@ -128,16 +134,30 @@ class TaskTest(TestCase):
         """
         Set up enivironment to test models
         """
-        self.feed1 = Feed(feed_url=reverse('planet:rss1'), name="rss1", short_name="rss1")
+        self.feed1 = Feed(
+            feed_url=reverse('planet:rss1'),
+            name="rss1",
+            short_name="rss1"
+        )
         self.feed1.save()
 
-        self.feed2 = Feed(feed_url=reverse('planet:rss2'), name="rss2", short_name="rss2")
+        self.feed2 = Feed(
+            feed_url=reverse('planet:rss2'),
+            name="rss2",
+            short_name="rss2"
+        )
         self.feed2.save()
 
-        self.post1 = Post(feed=self.feed1, link="http://localhost/post1")
+        self.post1 = Post(
+            feed=self.feed1,
+            link="http://localhost/post1"
+        )
         self.post1.save()
-        
-        self.post2 = Post(feed=self.feed2, link="http://localhost/post2")
+
+        self.post2 = Post(
+            feed=self.feed2,
+            link="http://localhost/post2"
+        )
         self.post2.save()
 
     def test_task_time(self):
@@ -159,7 +179,6 @@ class TaskTest(TestCase):
         result = entry_update_facebook(Post.objects.all()[0].id)
         self.assertEqual(result, True)
 
-
     def test_feed_refresh(self):
         feed = Feed.objects.all()[0]
         result = feed_refresh(feed.id)
@@ -177,6 +196,7 @@ class TaskTest(TestCase):
 
     def tearDown(self):
         pass
+
 
 class ViewsAnonymousTest(TestCase):
     """
@@ -222,7 +242,7 @@ class ViewsAnonymousTest(TestCase):
         """
         site-home
         ---------
-            :url: url(r'^site/$', SiteListView.as_view(), name="site-home"), 
+            :url: url(r'^site/$', SiteListView.as_view(), name="site-home"),
 
             Should return 200
         """
@@ -233,7 +253,11 @@ class ViewsAnonymousTest(TestCase):
         """
         site-submit
         -----------
-            :url: url(r'^site/submit/$', SiteSubmitWizardView.as_view(SiteSubmitForms), name="site-submit"), 
+            :url: url(
+                    r'^site/submit/$',
+                    SiteSubmitWizardView.as_view(SiteSubmitForms),
+                    name="site-submit"
+                  )
 
             Should return a form.
             Should accept a post.
@@ -243,7 +267,7 @@ class ViewsAnonymousTest(TestCase):
         """Assert the `submit` site is visible to anonymous users."""
         result = self.client.post(
             reverse('planet:site-submit'),
-            {'url': 'https://angry-planet.com/'}
+            {'url': 'http://spiegel.de/'}
         )
         self.assertEqual(result.status_code, 200)
         """Assert the `submit` site accepts `POST` from anonymous users."""
@@ -252,50 +276,88 @@ class ViewsAnonymousTest(TestCase):
         """
         site-add
         --------
-            :url: url(r'^site/add/$', SiteCreateView.as_view(), name="site-add"), 
+            :url: url(
+                    r'^site/add/$',
+                    SiteCreateView.as_view(),
+                    name="site-add"
+                    )
 
             To add a site should require credentials.
-            
+
             Should result in a redirect to the login-page.
         """
         result = self.client.get(reverse('planet:site-add'))
-        self.assertRedirects(result, '/accounts/login/?next=%s'%(reverse('planet:site-add')))
+        self.assertRedirects(
+            result,
+            '/accounts/login/?next=%s' % (reverse('planet:site-add'))
+        )
         self.assertEqual(result.status_code, 302)
 
     def site_view(self):
         """
         site-view
         ---------
-            :url: url(r'^site/(?P<pk>\d+)/$', SiteDetailView.as_view(), name="site-view"), 
+            :url: url(
+                r'^site/(?P<pk>\d+)/$',
+                SiteDetailView.as_view(),
+                name="site-view"
+                )
 
             Viewing a site should be available to the public.
 
             Should return 200.
         """
-        result = self.client.get(reverse('planet:site-view', args=str(self.site_id)))
+        result = self.client.get(
+            reverse('planet:site-view', args=str(self.site_id))
+        )
         self.assertEqual(result.status_code, 200)
 
     def site_update(self):
         """
         site-update
         -----------
-            :url: url(r'^site/(?P<pk>\d+)/update/$', SiteUpdateView.as_view(), name="site-update"),
+            :url: url(
+                    r'^site/(?P<pk>\d+)/update/$',
+                    SiteUpdateView.as_view(),
+                    name="site-update"
+                )
 
             .. todo:: needs to be defined.
         """
-        result = self.client.get(reverse('planet:site-update', args=str(self.site_id)))
-        self.assertRedirects(result, '/accounts/login/?next=%s'%(reverse('planet:site-update', args=str(self.site_id))))
+        result = self.client.get(
+            reverse(
+                'planet:site-update',
+                args=str(self.site_id)
+                )
+            )
+        self.assertRedirects(
+            result,
+            '/accounts/login/?next=%s' % (
+                reverse('planet:site-update',
+                        args=str(self.site_id)
+                        )
+            )
+        )
         self.assertEqual(result.status_code, 302)
 
     def site_delete(self):
         """
         site-delete
         -----------
-            :url: url(r'^site/(?P<pk>\d+)/delete/$', SiteDeleteView.as_view(), name="site-delete"), 
-            
+            :url: url(
+                r'^site/(?P<pk>\d+)/delete/$',
+                SiteDeleteView.as_view(),
+                name="site-delete"
+                )
+
             .. todo:: needs to be defined.
         """
-        result = self.client.get(reverse('planet:site-delete', args=str(self.site_id)))
+        result = self.client.get(
+            reverse(
+                'planet:site-delete',
+                args=str(self.site_id)
+            )
+        )
         self.assertEqual(result.status_code, 302)
 
     def test_site_views(self):
@@ -305,8 +367,8 @@ class ViewsAnonymousTest(TestCase):
 
         Test the :py:mod:`feeds.models.Site` section from the user-side.
 
-        Access the following pages, in order, as defined in :py:mod:`feeds.urls`.
-
+        Access the following pages, in order,
+        as defined in :py:mod:`feeds.urls`.
         """
         self.site_home()
         self.site_submit()
@@ -319,7 +381,11 @@ class ViewsAnonymousTest(TestCase):
         """
         feed-home
         ---------
-            :url: url(r'^list/$', FeedListView.as_view(), name="feed-home"), 
+            :url: url(
+                r'^list/$',
+                FeedListView.as_view(),
+                name="feed-home"
+                )
 
             Should return 200 for an anonymous user.
         """
@@ -330,32 +396,49 @@ class ViewsAnonymousTest(TestCase):
         """
         feed-home-paginated
         -------------------
-            :url: url(r'^page/(?P<page>\w+)/$', FeedListView.as_view(), name="feed-home-paginated"), 
+            :url: url(
+                     r'^page/(?P<page>\w+)/$',
+                     FeedListView.as_view(),
+                     name="feed-home-paginated"
+                  ),
 
             - Should return 200 for an anonymous user.
             - Should allow to navigate between paginated results.
         """
-        result = self.client.get(reverse('planet:feed-home-paginated', args=(1,)))
+        result = self.client.get(
+            reverse(
+                'planet:feed-home-paginated',
+                args=(1,)
+            )
+        )
         self.assertEqual(result.status_code, 200)
 
     def feed_add(self):
         """
         feed-add
         --------
-            :url: url(r'^add/$', FeedCreateView.as_view(), name="feed-add"), 
+            :url: url(r'^add/$', FeedCreateView.as_view(), name="feed-add"),
 
         """
         result = self.client.get(reverse('planet:feed-add'))
-        self.assertRedirects(result, '/accounts/login/?next=%s'%(reverse('planet:feed-add')))
+        self.assertRedirects(
+            result,
+            '/accounts/login/?next=%s' % (reverse('planet:feed-add'))
+        )
         self.assertEqual(result.status_code, 302)
 
     def feed_view(self):
         """
         feed-view
         ---------
-            :url: url(r'^(?P<pk>\d+)/$', FeedDetailView.as_view(), name="feed-view"), 
-            
-            Viewing details for a :py:mod:`feeds.models.Feed` should be available to the public.
+            :url: url(
+                r'^(?P<pk>\d+)/$',
+                FeedDetailView.as_view(),
+                name="feed-view"
+            ),
+
+            Viewing details for a :py:mod:`feeds.models.Feed` should be
+            available to the public.
 
             Should return 200.
 
@@ -369,28 +452,53 @@ class ViewsAnonymousTest(TestCase):
         """
         feed-update
         -----------
-            :url: url(r'^(?P<pk>\d+)/update/$', FeedUpdateView.as_view(), name="feed-update"),
+            :url:
+                url(
+                  r'^(?P<pk>\d+)/update/$',
+                  FeedUpdateView.as_view(),
+                  name="feed-update"
+                  )
 
             The `fixture` has a feed with the ID 1.
 
             .. todo:: needs to be defined.
         """
         result = self.client.get(reverse('planet:feed-update', args=(1,)))
-        self.assertRedirects(result, '/accounts/login/?next=%s'%(reverse('planet:feed-update', args=(1,))))
+        self.assertRedirects(
+            result,
+            '/accounts/login/?next=%s' % (
+                reverse(
+                    'planet:feed-update',
+                    args=(1,)
+                )
+            )
+        )
         self.assertEqual(result.status_code, 302)
 
     def feed_delete(self):
         """
         feed-delete
         -----------
-            :url: url(r'^(?P<pk>\d+)/delete/$', FeedDeleteView.as_view(), name="feed-delete"), 
-            
+            :url: url(
+            r'^(?P<pk>\d+)/delete/$',
+            FeedDeleteView.as_view(),
+            name="feed-delete"
+            ),
+
             The `fixture` has a feed with the ID 1.
 
             .. todo:: needs to be defined.
         """
         result = self.client.get(reverse('planet:feed-delete', args=(1,)))
-        self.assertRedirects(result, '/accounts/login/?next=%s'%(reverse('planet:feed-delete', args=(1,))))
+        self.assertRedirects(
+            result,
+            '/accounts/login/?next=%s' % (
+                reverse(
+                    'planet:feed-delete',
+                    args=(1,)
+                )
+            )
+        )
         self.assertEqual(result.status_code, 302)
 
     def test_feed_views(self):
@@ -401,7 +509,8 @@ class ViewsAnonymousTest(TestCase):
         Test Feed Views:
 
         .. todo::
-            url(r'^(?P<pk>\d+)/refresh/$', FeedRefreshView.as_view(), name="feed-refresh"), 
+            url(r'^(?P<pk>\d+)/refresh/$',
+            FeedRefreshView.as_view(), name="feed-refresh"),
         """
         self.feed_home()
         self.feed_home_paginated()
@@ -424,19 +533,35 @@ class ViewsLoggedInTest(TestCase):
         """
         Set up enivironment to test models
         """
-        self.user = User.objects.create_user(self.username, 'lennon@thebeatles.com', self.password)
+        self.user = User.objects.create_user(
+            self.username,
+            'lennon@thebeatles.com',
+            self.password
+        )
         """Test user."""
-        
-        self.feed1 = Feed(feed_url=reverse('planet:rss1'), name="rss1", short_name="rss1")
+
+        self.feed1 = Feed(
+            feed_url=reverse('planet:rss1'),
+            name="rss1",
+            short_name="rss1"
+        )
         self.feed1.save()
         """Feed 1."""
 
-        self.feed2 = Feed(feed_url=reverse('planet:rss2'), name="rss2", short_name="rss2")
+        self.feed2 = Feed(
+            feed_url=reverse('planet:rss2'),
+            name="rss2",
+            short_name="rss2"
+        )
         self.feed2.save()
         """Feed 2."""
 
         self.client = Client()
         """Test Client."""
+
+    def site_add(self):
+        """.. todo:: todo"""
+        pass
 
     def test_site(self):
         """
@@ -456,7 +581,8 @@ class ViewsLoggedInTest(TestCase):
         """
         go to feed-home
 
-        .. todo:: rename and restructure as in :py:mod:`feeds.tests.ViewsAnonymousTest`.
+        .. todo:: rename and restructure as in
+        :py:mod:`feeds.tests.ViewsAnonymousTest`.
         """
         result = self.client.get(reverse('planet:feed-home'))
         self.assertEqual(result.status_code, 200)
@@ -465,7 +591,12 @@ class ViewsLoggedInTest(TestCase):
         """
         go to feed-home-paginated
         """
-        result = self.client.get(reverse('planet:feed-home-paginated', args=("1",)))
+        result = self.client.get(
+            reverse(
+                'planet:feed-home-paginated',
+                args=("1",)
+            )
+        )
         self.assertEqual(result.status_code, 200)
 
     def test_feed_add(self):
@@ -493,8 +624,15 @@ class ViewsLoggedInTest(TestCase):
         permission = Permission.objects.get(codename="add_feed")
         self.user.user_permissions.add(permission)
         self.user.save()
-        self.client.login(username=self.username, password=self.password)
-        """.. todo:: this currently gives 'AttributeError: 'module' object has no attribute 'CRISPY_TEMPLATE_PACK'."""
+        self.client.login(
+            username=self.username,
+            password=self.password
+        )
+        """
+        .. todo:: this currently gives
+        'AttributeError: 'module' object has no
+        attribute 'CRISPY_TEMPLATE_PACK'.
+        """
         # result = self.client.get(reverse('planet:feed-add'), follow=False)
         # self.assertEqual(result.status_code, 200)
 
@@ -511,15 +649,6 @@ class ViewsLoggedInTest(TestCase):
         """
         result = self.client.post(reverse('planet:feed-add'))
         self.client.login(username=self.username, password=self.password)
-        self.assertEqual(result.status_code, 302)
-
-    def test_feed_add_post_valid_credential(self):
-        """
-        go to feed-add, anonymous client
-        """
-        result = self.client.post(reverse('planet:feed-add'))
-        self.client.login(username=self.username, password=self.password)
-        permission = Permission.objects.get(codename="add_feed")
         self.assertEqual(result.status_code, 302)
 
     def test_feed_view(self):
@@ -562,5 +691,30 @@ class ViewsLoggedInTest(TestCase):
         """Get first feed from the db. We use fixtures, so we can assume there are feeds."""
         with self.assertNumQueries(1):
             Post.objects.create(feed=feed)
+
+
+
+def TestSiteCredential(TestCase):
+    """
+    Test those aspects of :py:mod:`feeds.views` related to
+    py:mod:`feeds.models.Site`.
+    """
+    def setUp(self):
+        pass
+
+class TestFeedCredentials(TestCase):
+    """
+    Test those aspects of :py:mod:`feeds.views` related to
+    py:mod:`feeds.models.Feed`, that require proper cedentials.
+    """
+    def test_feed_add_post_valid_credential(self):
+        """
+        go to feed-add
+        """
+        result = self.client.post(reverse('planet:feed-add'))
+        self.client.login(username=self.username, password=self.password)
+        permission = Permission.objects.get(codename="add_feed")
+        self.assertEqual(result.status_code, 302)
+
 
 
