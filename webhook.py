6519ae05fa0be8e065b837db77ba3eb6057712b6
Andreas Neumeier <andreas@neumeier.org>
2014-05-11 11:01:52 +0200

    added webhook and call it from 'success' in travis.

---
+++ webhook.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ts=4 et sw=4 sts=4
+
+"""
+Call a webhook.
+"""
+
+import requests
+
+if __name__ == '__main__':
+    payload = {'key1': 'value1', 'key2': 'value2'}
+    r = requests.post("https:/angry-planet.com/hook/feeds", data=payload)
