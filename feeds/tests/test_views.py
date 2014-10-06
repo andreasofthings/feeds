ae242212cc38dbef9a663636d66300a2c84e827b
Andreas Neumeier <andreas@neumeier.org>
2014-10-06 22:03:15 +0200

    added another button to the category list, tried to fix a test

--- feeds/tests/test_views.py
+++ feeds/tests/test_views.py
@@ -27,7 +27,7 @@ class ViewsAnonymousTest(TestCase):
     .. moduleauthor:: Andreas Neumeier <andreas@neumeier.org>
     """
 
-    fixtures = ['Feed.yaml']
+    fixtures = ['Feed.yaml', 'Categories.yaml',]
 
     def setUp(self):
         """
