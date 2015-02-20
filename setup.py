75713e4735bd0299602cac15fdc5e9cfbbbbcb30
Andreas Neumeier <andreas@neumeier.org>
2015-02-20 13:20:09 +0100

    depends

--- setup.py
+++ setup.py
@@ -9,25 +9,28 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 from feeds import __version__
 
 setup(
-    name = 'feeds',
-    version = __version__,
-    packages = ['feeds'],
-    include_package_data = True,
-    license = 'BSD License', # example license
-    description = 'A feedburner replacement built on Django.',
-    long_description = README,
-    url = 'https://angry-planet.com/feeds',
-    author = 'Andreas.Neumeier',
-    author_email = 'andreas@neumeier.org',
-    classifiers = [
+    name='feeds',
+    version=__version__,
+    packages=['feeds'],
+    include_package_data=True,
+    license='BSD License',    # example license
+    description='A feedburner replacement built on Django.',
+    long_description=README,
+    url='https://angry-planet.com/feeds',
+    author='Andreas.Neumeier',
+    author_email='andreas@neumeier.org',
+    classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License', # example license
+        'License :: OSI Approved :: BSD License',    # example license
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+    depends=[
+        'feedparser>=5.1.2',
+    ]
 )
