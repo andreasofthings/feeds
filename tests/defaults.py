7c1ab689f9f4b321a87a146f59e5ecfe8c19571c
Andreas Neumeier <andreas@neumeier.org>
2015-03-28 20:39:07 +0100

    blah.

--- tests/defaults.py
+++ tests/defaults.py
@@ -104,7 +104,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap"
 # Haystack
 HAYSTACK_CONNECTIONS = {
     "default": {
-        'ENGINE': 'haystack.backends.whoosh_backend.ElasticsearchSearchEngine',
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
         'URL': 'http://127.0.0.1:9200/',
         'INDEX_NAME': 'haystack',
     }
