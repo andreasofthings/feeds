4a5d3f0634a0a35cd75994d4760ff1be508ddb8e
Andreas Neumeier <andreas@neumeier.org>
2015-08-15 22:07:27 +0200

    bla.

--- feeds/mixins.py
+++ feeds/mixins.py
@@ -54,7 +54,7 @@ class UserAgentRequiredMixin(UserPassesTestMixin):
 class PaginationMixin(object):
     def get_paginate_by(self, queryset):
         if 'paginate_by' in self.request.GET:
-            return int(getattr('paginate_by', self.request.GET, 10))
+            return int(getattr(self.request.GET, 'paginate_by', 10))
         # elif type(self.request.user) is not AnonymousUser:
         #    if 'number_initially_displayed' in self.request.user.options:
         #        return self.request.user.options.number_initially_displayed
