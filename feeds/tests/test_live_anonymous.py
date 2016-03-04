3077989dbd0d9e8540943f4e735dfa401dd22041
Andreas Neumeier <andreas@neumeier.org>
2016-03-04 20:24:01 +0100

    Started Moar Seleneium Tests.

---
+++ feeds/tests/test_live_anonymous.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ts=4 et sw=4 sts=4
+
+"""
+"""
+
+from django.test import LiveServerTestCase
+from django.core.urlresolvers import reverse
+
+from selenium import webdriver
+
+
+class TestAllViewsAnonymousLive(LiveServerTestCase):
+    """
+    Test options with browser.
+    """
+    username = "john"
+    realname = "John Lennon"
+    password = "password"
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def test_anonymous(self):
+        """
+        Test the home page from browser.
+        """
+        self.browser.get(self.live_server_url + reverse('planet:home'))
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Feeds', body.text)
+
+        self.browser.get(self.live_server_url + reverse('planet:site-home'))
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Feeds', body.text)
+
+        self.browser.get(self.live_server_url + reverse('planet:feed-home'))
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Feeds', body.text)
+
+        self.browser.get(self.live_server_url + reverse('planet:post-home'))
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Feeds', body.text)
+
+    def tearDown(self):
+        self.browser.quit()
