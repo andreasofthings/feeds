286e21065372497c19f9a75e237bb23e5ad6ae77
Andreas Neumeier <andreas@neumeier.org>
2016-03-08 11:10:07 +0100

    python 3 compat.

--- tests/travis-settings.py
+++ tests/travis-settings.py
@@ -1,4 +1,4 @@
-from defaults import *
+from .defaults import *
 
 # Database for travis
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
@@ -17,7 +17,8 @@ DATABASES = {
 # Haystack
 HAYSTACK_CONNECTIONS = {
     "default": {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'ENGINE':
+        'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
         'URL': 'http://127.0.0.1:9200/',
         'INDEX_NAME': 'haystack',
     }
