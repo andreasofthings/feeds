4b5c37697e3ea262a5001a141684157d86ac3b3f
Andreas Neumeier <andreas@neumeier.org>
2017-02-17 17:33:31 +0100

    move out api-auth to project level.

--- feeds/api/urls.py
+++ feeds/api/urls.py
@@ -17,10 +17,4 @@ router.register(r'posts', PostViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(
-        r'^api-auth/',
-        include(
-            'rest_framework.urls',
-            namespace='rest_framework')
-    ),
 ]
