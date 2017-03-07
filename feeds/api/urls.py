d61e4d17220193d4d901635f0659665f207920fa
Andreas Neumeier <andreas@neumeier.org>
2017-03-07 10:17:31 +0100

    Update urls.py

--- feeds/api/urls.py
+++ feeds/api/urls.py
@@ -7,16 +7,16 @@ from django.conf.urls import url, include
 
 from rest_framework import routers
 
-from .views import SiteViewSet
 from .views import FeedViewSet
 from .views import PostViewSet
+from .views import WebSiteViewSet
 from .views import CategoryViewSet
 
 router = routers.DefaultRouter()
 # register job endpoint in the router
-router.register(r'sites', SiteViewSet)
 router.register(r'feeds', FeedViewSet)
 router.register(r'posts', PostViewSet)
+router.register(r'websites', WebSiteViewSet)
 router.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
