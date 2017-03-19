23d9a715c2a2481c8898907d019aa92db86d64a8
Andreas Neumeier <andreas@neumeier.org>
2017-03-19 13:01:00 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,12 +16,12 @@ import os
 import django
 os.environ['DJANGO_SETTINGS_MODULE'] = "tests.settings"
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-django.setup()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+django.setup()
 
 # -- General configuration ----------------------------------------------------
 
