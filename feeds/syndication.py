0df981329e6c6bc13ce53d5960af3c2d4d11b034
Andreas Neumeier <andreas@neumeier.org>
2017-04-19 18:28:51 +0200

    oh my

--- feeds/syndication.py
+++ feeds/syndication.py
@@ -16,4 +16,4 @@ class LatestEntriesFeed(Feed):
 
     # item_link is only needed if NewsItem has no get_absolute_url method.
     def item_link(self, item):
-        return reverse('post-detail', args=[item.pk])
+        return reverse('feeds:post-detail', args=[item.pk])
